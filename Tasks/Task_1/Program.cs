696a851e9e7f422705d5eb7784f85e9ac2af4c5e
Volodya <volodya.atoyan@list.ru>
2022-12-24 16:23:49 +0300

    Added Task_1

---
+++ Tasks/Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите трёхзначное  число ");
+int number = Convert.ToInt32(ReadLine());
+
+int secomd = number/10;
+int secomd2 = secomd%10;
+
+WriteLine(secomd2);
