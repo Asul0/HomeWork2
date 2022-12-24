d64b57ab3143881df3eec4b722cc137c0a01baab
Volodya <volodya.atoyan@list.ru>
2022-12-24 16:27:06 +0300

    Added Task_3

---
+++ Tasks/Task_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите номер дня недели ");
+int numDay = Convert.ToInt32(ReadLine());
+
+if (numDay == 1)
+{
+    WriteLine("нет");
+}
+if (numDay == 2)
+{
+    WriteLine("нет");
+} 
+if (numDay == 3)
+{
+    WriteLine("нет");
+} 
+if (numDay == 4)
+{
+    WriteLine("нет");
+} 
+if (numDay == 5)
+{
+    WriteLine("нет");
+} 
+if (numDay == 6)
+{
+    WriteLine("да");
+} 
+if (numDay == 7)
+{
+    WriteLine("да");
+}
+if (numDay > 7)
+{
+    WriteLine("по данному номеру нет дня недели");
+}
