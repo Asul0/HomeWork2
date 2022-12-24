38e160f0e8c297f2c366466468f614e8a3ebf6a2
Volodya <volodya.atoyan@list.ru>
2022-12-24 16:25:42 +0300

    Added Task_2

---
+++ Tasks/Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число ");
+int number = Convert.ToInt32(ReadLine());
+int result = number%10;
+
+if (number > 99)
+{
+    WriteLine(result);
+}
+else
+{
+    WriteLine("третьей цифры нет");
+}
